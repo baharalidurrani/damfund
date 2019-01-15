fe5ccc32cb9eb5b296693e9ccd3479beee96c006
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-01-15 23:17:17 +0500

    puppeteer flow made | variables getting logic to next

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,8 +1,29 @@
 exports.get = (req, res) => {
-    crawl(req, res);
-    // res.send("hello world");
+    const scrapPromise = crawl();
+    Promise.resolve(scrapPromise).then((data) => {
+        res.send(data);
+    });
 };
 
-crawl = (req, res) => {
+async function crawl() {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({
+        // dumpio: true,
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto('http://scp.gov.pk/popup.aspx');
 
+    await page.screenshot({
+        path: 'example.png'
+    });
+
+    const finalJSON = {
+        dates: [],
+        funds: []
+    }
+    //////////////logic
+
+    await browser.close();
+    return finalJSON;
 }
