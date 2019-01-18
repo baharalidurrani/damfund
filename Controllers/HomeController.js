22733c5e529ec2f0a42c7ef1ae422a0ea429fec4
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-01-18 19:00:12 +0500

    console log formated for home route | also another puppeteer buildpack is added for heroku

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -3,7 +3,8 @@ exports.get = (req, res) => {
     const scrapPromise = scraper.crawl();
 
     Promise.resolve(scrapPromise).then((data) => {
-        console.log(data);
+        console.log("Total entries: " + data.dates.length);
+        console.log("Total Funds: " + data.funds[data.funds.length - 1]);
         res.send(data);
     });
 };
