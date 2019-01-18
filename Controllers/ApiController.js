33cd9dc7fae212cbe4ad65c4f7e07c8fa8d4dbb9
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-01-18 12:53:35 +0500

    exception added

--- Controllers/ApiController.js
+++ Controllers/ApiController.js
@@ -1,9 +1,14 @@
 exports.get = (req, res) => {
-    const scraper = require('./scraper');
-    const scrapPromise = scraper.crawl();
+    try {
+        const scraper = require('./scraper');
+        const scrapPromise = scraper.crawl();
 
-    Promise.resolve(scrapPromise).then((data) => {
-        console.log(data);
-        res.json(data);
-    });
+        Promise.resolve(scrapPromise).then((data) => {
+            console.log(data);
+            res.json(data);
+        });
+    } catch (e) {
+        console.log(e);
+        res.send("Problem with in the scraper method!");
+    }
 };
