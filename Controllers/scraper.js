9d59832e4b61223cee44e589920816f7e7f6e349
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-01-18 17:58:28 +0500

    add an args for puppeteer-heroku-buildpack

--- Controllers/scraper.js
+++ Controllers/scraper.js
@@ -1,7 +1,11 @@
 async function crawl() {
     const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
+        //for puppeteer-heroku-buildpack
+        args: ['--no-sandbox'],
+        //for piping chromium console output to this NodeJS console
         dumpio: true,
+        //for chromium GUI turning ON/Off
         headless: true
     });
 
