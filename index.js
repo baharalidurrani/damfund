333e33970f0910634686e35eafb49bd9e6db8ef9
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-01-21 01:08:25 +0500

    external css added | static files added in index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 
 //setting template engine
 app.set('view engine', 'ejs');
+//static files
+app.use(express.static('./views/assets'));
 
 //start web server
 app.listen(port, () => {
